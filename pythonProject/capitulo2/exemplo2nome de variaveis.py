fb573e18db9fb7f433b7d96f9d1f6027306e3b18
pedrocapa22 <pedro.omura.freitas@escola.pr.gov.br>
2025-05-06 12:33:30 -0300

    capitulo2 expressoes e instrucoes

---
+++ pythonProject/capitulo2/exemplo2nome de variaveis.py
@@ -0,0 +1,6 @@
+ #variaveis nao podenm começar com numeros e letra maiuscula
+ # 76trombones  =  "big parade"
+ #More = 10000
+
+ #variaveis nao podem ter caracteres epeciais tirando o "_"
+ #more*=4378
---
+++ pythonProject/capitulo2/exemplo3expressoeseinstrucoes.py
@@ -0,0 +1,4 @@
+n =  12
+w = 2
+total =  n + w
+print (total)
