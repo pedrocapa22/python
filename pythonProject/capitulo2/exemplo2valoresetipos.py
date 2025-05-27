a5f04fee672fa29a59b0a570eed09752ecf9e082
pedrocapa22 <pedro.omura.freitas@escola.pr.gov.br>
2025-05-27 11:36:47 -0300

    exercicio1

---
+++ pythonProject/capitulo2/exemplo2valoresetipos.py
@@ -0,0 +1,21 @@
+#tipo inteiro: 1,2,3... -2,-3-4
+inteiro = 2
+print(type(inteiro))
+
+#tipo real ou tempo flutuante, float   1,3,0,6........... -3 156
+
+float  = 1.3
+print(type(float))
+
+#caracteres
+
+char1= "a"
+char2= "2"
+
+print(type(char1))
+
+
+#string conjunto de caracteres
+
+stringnome = "pedro omura"
+print(type(stringnome))
---
+++ pythonProject/capitulo2/exercicio1.py
@@ -0,0 +1,7 @@
+fracao = 4/3
+pi = 3.14
+r = 5 ** 3
+
+volume = fracao*pi *r
+
+print("o volume de uma esfera de raio 5 é:'", volume)
